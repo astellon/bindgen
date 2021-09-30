d64263176ac7568c607d41757d94c445965abd8f
astellon <22632787+astellon@users.noreply.github.com>
2021-09-30 21:43:07 +0900

    Fixed an error on testing in Debian
    Updated the image as libyaml >= 0.2.2 is required.

--- ci/install_debian.sh
+++ ci/install_debian.sh
@@ -6,14 +6,7 @@ set -e
 set -x
 
 apt-get update
-apt-get install --yes apt-transport-https curl tzdata gnupg
-
-cat <<EOF > /etc/apt/sources.list.d/clang.list
-deb http://apt.llvm.org/${DISTRIB_CODENAME}/ llvm-toolchain-${DISTRIB_CODENAME}-${CLANG_VERSION} main
-deb-src http://apt.llvm.org/${DISTRIB_CODENAME}/ llvm-toolchain-${DISTRIB_CODENAME}-${CLANG_VERSION} main
-EOF
-
-curl -fsSL https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
+apt-get install --yes curl tzdata
 
 # following https://crystal-lang.org/install/on_ubuntu/
 curl -fsSL https://crystal-lang.org/install.sh | bash -s -- --version=latest
@@ -22,6 +15,5 @@ apt-get update
 apt-get install --yes \
   build-essential git \
   crystal libxml2-dev zlib1g-dev libncurses-dev libgc-dev libyaml-dev \
-  llvm-${CLANG_VERSION} llvm-${CLANG_VERSION}-dev \
-  clang-${CLANG_VERSION} libclang-${CLANG_VERSION}-dev \
+  llvm llvm-dev clang libclang-dev \
   libpcre3-dev cmake libedit-dev
