c1442d8f6a253492a5b2588af6d26243a1d420ab
astellon <22632787+astellon@users.noreply.github.com>
2021-09-30 20:33:55 +0900

    Make CI runnable
    This commit makes the Docker containers buildable and allows us to run automated tests. However, it does not pass the test correctly in Debian and Arch.

--- ci/install_debian.sh
+++ ci/install_debian.sh
@@ -6,29 +6,21 @@ set -e
 set -x
 
 apt-get update
-apt-get install --yes apt-transport-https curl
+apt-get install --yes apt-transport-https curl tzdata gnupg
 
 cat <<EOF > /etc/apt/sources.list.d/clang.list
 deb http://apt.llvm.org/${DISTRIB_CODENAME}/ llvm-toolchain-${DISTRIB_CODENAME}-${CLANG_VERSION} main
 deb-src http://apt.llvm.org/${DISTRIB_CODENAME}/ llvm-toolchain-${DISTRIB_CODENAME}-${CLANG_VERSION} main
 EOF
 
-cat <<EOF > /etc/apt/sources.list.d/crystal.list
-deb https://dist.crystal-lang.org/apt crystal main
-EOF
+curl -fsSL https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
+
+# following https://crystal-lang.org/install/on_ubuntu/
+curl -fsSL https://crystal-lang.org/install.sh | bash -s -- --version=latest
 
 apt-get update
-
-if [ ! -f /etc/lsb-release ]; then
-  # Hack to detect Debian, which doesn't ship with a GPG installation.
-  apt-get install gnupg --yes
-fi
-
-# apt-key adv --keyserver keys.gnupg.net --recv-keys 09617FD37CC06B54 15CF4D18AF4F7421
-curl -sL "https://keybase.io/crystal/pgp_keys.asc" | apt-key add -
-
-apt-get install --yes --allow-unauthenticated \
-  build-essential \
+apt-get install --yes \
+  build-essential git \
   crystal libxml2-dev zlib1g-dev libncurses-dev libgc-dev libyaml-dev \
   llvm-${CLANG_VERSION} llvm-${CLANG_VERSION}-dev \
   clang-${CLANG_VERSION} libclang-${CLANG_VERSION}-dev \
